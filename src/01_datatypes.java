db80e7c5984b53b1d6bbe14156b702b8f6ca1e2a
Jagriti Priya <jagritidevelop21@gmail.com>
2026-02-10 21:20:44 +0000

    datatypes

---
+++ src/01_datatypes.java
@@ -0,0 +1,26 @@
+public class Main{
+    public static void main(String[] args){
+
+        int a = 23;
+        int b = 45;
+        int sum = a+b;
+        System.out.println(sum);
+
+        boolean option = true;
+        System.out.println(option);
+
+        char letter = 'a';
+        System.out.println(letter);
+    }
+
+}
+// java is statically typed need to declare 
+// variable type before use
+
+// Primitive Datatype
+// Integral : byte short int long
+// Floats : float double 
+// Char 
+// Boolean
+// Literals : constant value
+// Keywords reserved words used by compiler
